e0d4951fcdbccad139eb2c8ce299794c2edbd7b7
Ocean <dvsocean@icloud.com>
2018-08-09 15:10:47 -0700

    client microservice start point

---
+++ src/main/java/com/subClassed/inherited/InheritedApplication.java
@@ -0,0 +1,12 @@
+package com.subClassed.inherited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InheritedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InheritedApplication.class, args);
+	}
+}
---
+++ src/test/java/com/subClassed/inherited/InheritedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.subClassed.inherited;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InheritedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
